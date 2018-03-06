a81bdb2238bd6dc60c066c71fa2eddc50177e4b1
Lauris Vāvere <lauris@ma-1.lv>
2018-03-06 17:10:17 +0200

    precizēta uathor izlaišana

--- assets/build/plugins/redmine_mail_from/lib/redmine_mail_from/mailer_model_patch.rb
+++ assets/build/plugins/redmine_mail_from/lib/redmine_mail_from/mailer_model_patch.rb
@@ -16,49 +16,51 @@ module RedmineMailFrom
 
       Rails.logger.info "mail with patch"
 
-      return if not @author
+      if @author
 
-      Rails.logger.info "author: #{@author.login}"
+        Rails.logger.info "author: #{@author.login}"
 
-      placeholder = {
-        '%f' => @author ? @author.firstname : nil,
-        '%l' => @author ? @author.lastname : nil,
-        '%m' => @author ? @author.mail : nil,
-        '%u' => @author ? @author.login : nil
-      }
+        placeholder = {
+          '%f' => @author ? @author.firstname : nil,
+          '%l' => @author ? @author.lastname : nil,
+          '%m' => @author ? @author.mail : nil,
+          '%u' => @author ? @author.login : nil
+        }
 
-      Rails.logger.info "author mail: #{@author.mail}"
+        Rails.logger.info "author mail: #{@author.mail}"
 
-      from = ''
+        from = ''
 
-      Setting.mail_from.split(/\s*::\s*/).each do |s|
-        nerr = 0
+        Setting.mail_from.split(/\s*::\s*/).each do |s|
+          nerr = 0
 
-        placeholder.each do |key, val|
-          next unless s.match(/#{key}/)
+          placeholder.each do |key, val|
+            next unless s.match(/#{key}/)
 
-          if val.nil? then nerr += 1 end
+            if val.nil? then nerr += 1 end
 
-          s.gsub!(/#{key}/, val || '')
+            s.gsub!(/#{key}/, val || '')
+          end
+
+          from = s
+
+          break if nerr == 0
         end
 
-        from = s
+        Rails.logger.info "mail from: #{from}"
 
-        break if nerr == 0
+        host = Setting.host_name.split(/[\/:]/).first
+
+        if @issue
+          listid = "<#{@issue.project.identifier}.#{host}>"
+        else
+          listid = "<#{host}>"
+        end
+
+        headers['From'] = from
+        headers['List-Id'] = listid
+
       end
-
-      Rails.logger.info "from: #{from}"
-
-      host = Setting.host_name.split(/[\/:]/).first
-
-      if @issue
-        listid = "<#{@issue.project.identifier}.#{host}>"
-      else
-        listid = "<#{host}>"
-      end
-
-      headers['From'] = from
-      headers['List-Id'] = listid
 
       mail_without_patch(headers, &block)
     end
